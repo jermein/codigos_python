339565c730232d3eb66496430608540ddf99a5a9
jermein <110571749+jermein@users.noreply.github.com>
2022-08-13 08:18:42 -0500

    Update clase.py

--- clase.py
+++ clase.py
@@ -1,12 +1,19 @@
 class celular:
+    #en este caso self hace referencia al objeto en especifico
+    #init es para crear la instancia
     def __init__(self, marca, modelo):
         self.marca = marca
         self.modelo = modelo
         self.prendido = False
+        self.aplicacio = False
     
     def prender(self):
         self.prendido = True
         print("El", self.marca, self.modelo, "se ha arrancado")
+    
+    def aplicacion(self):
+        self.appliacionx = True
+        print("la aplicacion se ha arrancado")
     
     def apagado(self):
         self.prendido = False
@@ -17,6 +24,7 @@ iphone = celular("iphone", "model 13")
 
 print(type(samsung))
 print(type(iphone))
+
 print(samsung.marca, samsung.modelo)
 print(iphone.marca, iphone.modelo)
 
@@ -26,7 +34,15 @@ iphone.prender()
 print(samsung.prender)
 print(iphone.prender)
 
+samsung.aplicacion()
+iphone.aplicacion()
+
+print(samsung.aplicacion)
+print(iphone.aplicacion)
+
 samsung.apagado()
 iphone.apagado()
+
 print(samsung.apagado)
 print(iphone.apagado)
+
