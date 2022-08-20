19bf528f524cadcf6f3695dd7254eb16d79e43aa
jermein <110571749+jermein@users.noreply.github.com>
2022-08-20 11:07:21 -0500

    Add files via upload

---
+++ return.py
@@ -0,0 +1,27 @@
+#recurcion
+#n = int(input("digite numero"))
+#def factorial(n):  
+    #if n == 1:
+        #return n
+    #return n * factorial(n-1) 
+#print(factorial(n))
+
+#def multiplicacion(n,n1):
+    #if n1 == 0:
+        #return n1
+    #return n + multiplicacion(n,n1-1)
+#print(multiplicacion(5,5))
+
+
+#def division(n,n1):
+    #if n < n1:
+        #return 0
+    #return 1 + division(n-n1, n1)
+
+#print(division(12,2))
+
+
+
+
+
+
